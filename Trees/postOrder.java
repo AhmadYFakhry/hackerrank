b99897ed3034c81698648fc59a7c42d316a20ad5
Ahmad Fakhry <afakhry98@gmail.com>
2019-09-29 20:32:27 -0400

    Fixed post order

--- Trees/postOrder.java
+++ Trees/postOrder.java
@@ -24,11 +24,39 @@ class Solution {
 
     public static void postOrder(Node root) {
         // Base Case
-        if(root == null) return;
+        if (root == null)
+            return;
         // Recursion step
         postOrder(root.left);
         postOrder(root.right);
         System.out.print(root.data + " ");
     }
 
-public static Node insert(Node root, int data) {
+    public static Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if (data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while (t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        postOrder(root);
+    }
+}

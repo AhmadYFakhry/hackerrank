1293502c33f1082db5edb08a85d0ef768ee5f853
Ahmad Fakhry <afakhry98@gmail.com>
2019-10-13 14:29:58 -0400

    Fixed solutions using BigInt

--- Algorithms/Implementation/extraLongFactorials.js
+++ Algorithms/Implementation/extraLongFactorials.js
@@ -1,29 +1,44 @@
-// Iterative
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Recursive 
 function extraLongFactorials(n) {
-    let longInt = 1;
-    let i = 1;
-    while(i <= n) {
-        longInt = i * longInt;
-        i++;
-    }
-    return longInt;
+    if(n == 1) return BigInt(1);
+    return BigInt(n) * extraLongFactorials(BigInt(n) - BigInt(1));
 }
-
-// Recursive
-function extraLongFactorials1(n) {
-    if(n === 1) {
-        return 1;
-    }
-    return n * extraLongFactorials(n - 1);
-}
-
-// Solutions done by importing bignumber 
 // Iterative
-function extraLongFactorial2(n) {
-    let bn = require('bignumber.js');
-    let result = new bn(1);
-    for(let i = 1; i <= n; i++) {
-        result = result.mul(i);
+function extraLongFactorials1(n) {
+    const bigN = BigInt(n);
+    let result = BigInt(1); // 1n also acceptable
+    for(let i = BigInt(0); i < n; i++) {
+        result *= bigN - i;
     }
-    return(result.toFixed());
+    return result;
+}
+
+function main() {
+    const n = parseInt(readLine(), 10);
+
+    console.log(extraLongFactorials(n).toString())
 }
